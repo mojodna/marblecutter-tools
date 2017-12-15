fe4263730730f2a2d183e8e82d5a392fccbba358
Seth Fitzsimmons <seth@mojodna.net>
2017-12-15 10:10:56 -0800

    Support for TIFFs in uncompressed tar archives
    ArcticDEM is distributed this way.

--- bin/transcode.sh
+++ bin/transcode.sh
@@ -54,6 +54,15 @@ elif [[ "$input" =~ \.tar\.gz$ ]]; then
   fi
 
   input="tar://${input}!${inner_source}"
+elif [[ "$input" =~ \.tar$ ]]; then
+  inner_source=$(tar tf ${input} | grep "tif$" | head -1)
+
+  if [[ -z "$inner_source" ]]; then
+    >&2 echo "Could not find a TIFF inside ${input}"
+    exit 1
+  fi
+
+  input="tar://${input}!${inner_source}"
 fi
 
 >&2 echo "Transcoding ${input}..."
